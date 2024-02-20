ec9fc2c146c0b3a6867af54178df602965a581ff
Anissa Nishioka <ani.nishioka@gmail.com>
2024-02-20 14:53:24 -0800

    implement serial class

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,20 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        """Create serial generator instance."""
+        self.start = start
+        self.currentNum = start
+
+    def __repr__(self):
+        """Return representation of serial generator instance."""
+        return f"<SerialGenerator start={self.start} currentNum={self.currentNum}>"
+
+    def generate(self):
+        """Return next sequential number and increment self.currentNumber."""
+        self.currentNum += 1
+        return self.currentNum - 1
+
+    def reset(self):
+        """Reset current number to self.start."""
+        self.currentNum = self.start
